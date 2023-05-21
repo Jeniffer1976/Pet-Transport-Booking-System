17511cf903b1df077b63f7fe2b3c25198214c476
Jeniffer1976 <21025290@myrp.edu.sg>
2023-05-21 16:19:04 +0800

    removed comments

--- dbFunctions.php
+++ dbFunctions.php
@@ -11,12 +11,8 @@ $db_username = "root";
 $db_password = "";
 $db_name = "c203_wagginwheelsdb";
 // 
-$link = mysqli_connect($db_host, $db_username, $db_password, $db_name);
 
-// $db_host = "localhost";
-// $db_username = "root";
-// $db_password = "";
-// $db_name = "id20783214_wagginwheeldb";
+$link = mysqli_connect($db_host, $db_username, $db_password, $db_name);
 
 if (!$link) {
     die(mysqli_error($link));
@@ -24,21 +20,5 @@ if (!$link) {
     // die(mysqli_connect_error());
 }
 
-// function login($username, $password)
-// {
-//     global $link;
-//     $queryCheck = "SELECT * FROM users
-//           WHERE username='$username'
-//           AND password = '$password'";
 
-//     $resultCheck = mysqli_query($link, $queryCheck) or die(mysqli_error($link));
-
-//     if (mysqli_num_rows($resultCheck) == 1) {
-//         header("Location: index.php"); 
-//         exit();
-
-//     } else {
-//         return "That's not the right username or password. <br>Please try again";
-//     }
-// }
 ?>
