69f0ad6f489a4849f6d2785e11cc5fed6e90a070
Hanizalyssa <20007201@myrp.edu.sg>
2023-07-05 03:20:10 +0800

    haniz home page

--- index.php
+++ index.php
@@ -35,22 +35,21 @@
 
     <!-- Opening -->
     <br>
-        <div class="container">
-            <h1 class="header1">Never leave your FURiends behind</h1>
-            <div align="center">
-                <img src="images/dogshome.jpeg" id="homeimg">
-                <div class="childClass firstchild">
-                    <div class="text1">
-                        <h2>Provide your <br>
-                            pets with the best <br>
-                            comfort and care <br>
-                            as they travel!
-                        </h2>
-                    </div>
+        <div class="container col-xxl-10 px-4 py-2">
+            <div class="row flex-lg-row-reverse align-items-center g-5 py-5">
+                <div class="col-10 col-sm-8 col-lg-6">
+                    <img src="images/homepagepets.png" class="d-block mx-lg-auto img-fluid" width="700" height="500"
+                        loading="lazy">
                 </div>
-                <div class="childClass1 secondchild">
-                    <div>
-                        <button type="button" class="btn rounded-pill btnPrimary2 text">Request a quote</button>
+                <div class="col-lg-6">
+                    <h1 class="font1 display-4 fw-bold lh-1 mb-3">Never leave your FURiends behind</h1>
+                    <h5 class="font2 display-8 fw-bold lh-1 mb-3">Provide your pets with the best comfort <br>
+                        and care as they travel!</h5>
+                    <p class="text3">Trust us with your pet's transportation needs, and we'll go above and beyond to ensure their safety, comfort, and happiness. Request a qoute today to discuss your requirements and let us take care of the logistics while you focus on creating wonderful memories with your furry friends.
+                    </p>
+                    <div class="d-grid gap-2 d-md-flex justify-content-md-start">
+                        <button type="button" class="btn rounded-pill btn-primary btn-lg px-5 me-md-2">Request a
+                            qoute</button>
                     </div>
                 </div>
             </div>
@@ -60,9 +59,97 @@
 
 
     <!-- Testimonials -->
-    <hr>
-    <div class="container">
-        <h1 class="header1">Testimonials</h1>
+    <div id="myCarousel" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-indicators">
+            <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="0" class="active" aria-label="Slide 1"
+                aria-current="true"></button>
+            <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="1" aria-label="Slide 2"
+                class=""></button>
+            <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="2" aria-label="Slide 3"
+                class=""></button>
+        </div>
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <svg class="bd-placeholder-img" width="100%" height="70%" aria-hidden="true"
+                    preserveAspectRatio="xMidYMid slice" focusable="false">
+                    <rect></rect>
+                </svg>
+
+                <div class="container">
+                    <div class="carousel-caption">
+                        <h1 class="header1">Testimonials</h1>
+                        <img class="avatar" src="images/indianguy.png">
+                        <h2 class="font4">From the moment I contacted them, <br>
+                            their team was professional, caring, and efficient.</h2>
+                        <p class="text3">"I recently used Waggin' Wheels to move my beloved dog across the country,
+                            and I couldn't be happier with their services. From the moment I contacted them,
+                            their team was professional, caring, and efficient. They handled all the necessary
+                            paperwork,
+                            ensured my dog's safety during the journey, and provided regular updates along the way.
+                            My furry friend arrived happy and healthy, and I highly recommend Waggin' Wheels to anyone
+                            in need of reliable and trustworthy pet transportation."</p>
+                        <p class="text3">- Kumar Rajesh, Member since 2019</p>
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <svg class="bd-placeholder-img" width="100%" height="70%" aria-hidden="true"
+                    preserveAspectRatio="xMidYMid slice" focusable="false">
+                    <rect></rect>
+                </svg>
+
+                <div class="container">
+                    <div class="carousel-caption">
+                        <h1 class="header1">Testimonials</h1>
+                        <img class="avatar" src="images/indianguy.png">
+                        <h2 class="font4">From the moment I contacted them, <br>
+                            their team was professional, caring, and efficient.</h2>
+                        <p class="text3">"I recently used Waggin' Wheels to move my beloved dog across the country,
+                            and I couldn't be happier with their services. From the moment I contacted them,
+                            their team was professional, caring, and efficient. They handled all the necessary
+                            paperwork,
+                            ensured my dog's safety during the journey, and provided regular updates along the way.
+                            My furry friend arrived happy and healthy, and I highly recommend Waggin' Wheels to anyone
+                            in need of reliable and trustworthy pet transportation."</p>
+                        <p class="text3">- Kumar Rajesh, Member since 2019</p>
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <svg class="bd-placeholder-img" width="100%" height="70%" aria-hidden="true"
+                    preserveAspectRatio="xMidYMid slice" focusable="false">
+                    <rect></rect>
+                </svg>
+
+                <div class="container">
+                    <div class="carousel-caption">
+                        <h1 class="header1">Testimonials</h1>
+                        <img class="avatar" src="images/indianguy.png">
+                        <h2 class="font4">From the moment I contacted them,
+                            their team was <br> professional, caring, and efficient.</h2>
+                        <p class="text3">"I recently used Waggin' Wheels to move my beloved dog across the country,
+                            and I couldn't be happier with their services. From the moment I contacted them,
+                            their team was professional, caring, and efficient. They handled all the necessary
+                            paperwork,
+                            ensured my dog's safety during the journey, and provided regular updates along the way.
+                            My furry friend arrived happy and healthy, and I highly recommend Waggin' Wheels to anyone
+                            in need of reliable and trustworthy pet transportation."</p>
+                        <p class="text3">- Kumar Rajesh, Member since 2019</p>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#myCarousel" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#myCarousel" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
     </div>
     <!--  -->
 
