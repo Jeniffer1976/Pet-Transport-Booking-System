76c25ecea415bdb4f7f4ca893f8a9b2f6e3780ff
LeaDeLeaf <104687952+LeaDeLeaf@users.noreply.github.com>
2023-07-12 03:36:43 +0800

    Created sidebar and admin page

---
+++ admin_quotes.php
@@ -0,0 +1,101 @@
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Quotes | Waggin Wheels</title>
+
+    <link rel="icon" type="image/x-icon" href="images/logoNoText.ico">
+
+    <!--Bootstrap CSS link take note of version-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+
+    <!--Boostrap JS link-->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe"
+        crossorigin="anonymous"></script>
+
+    <!--Font Awesome-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+
+    <!-- Stylesheets -->
+
+    <link rel="stylesheet" href="stylesheets/common.css">
+    <link rel="stylesheet" href="stylesheets/admin.css">
+    <link rel="stylesheet" href="stylesheets/responsive.css">
+</head>
+
+<body>
+
+    <!-- Navbar -->
+    <?php include("navbar_admin.php") ?>
+    <!--  -->
+
+    <!-- Header -->
+    <!-- <h1 class="header1">CUSTOMER QUOTES</h1> -->
+    <!-- -->
+
+    <!-- Sidebar -->
+    <div class="navcontainer">
+        <nav class="nav">
+            <div class="nav-upper-options">
+                <div class="nav-option active">
+                    <a class="nav-link nav-text" href="admin.php">
+                        <i class="fa-solid fa-gauge"></i>
+                        Dashboard
+                    </a>
+
+                </div>
+
+                <div class="option2 nav-option">
+                    <a class="nav-link nav-text" href="admin_quotes.php">
+                        <i class="fa-solid fa-table"></i>
+                        Quotes
+                    </a>
+
+                </div>
+
+                <div class="nav-option option3">
+                    <a class="nav-link nav-text" href="editAdmin.php">
+                        <i class="fa-solid fa-user-gear"></i>
+                        Administrators
+                    </a>
+
+                </div>
+
+                <div class="nav-option option4">
+                    <a class="nav-link nav-text" href="editCustomers.php">
+                        <i class="fa-solid fa-user"></i>
+                        Customers
+                    </a>
+
+                </div>
+
+            </div>
+        </nav>
+    </div>
+    <!-- -->
+
+    <!-- Table -->
+
+    <!-- -->
+
+    <!-- Footer -->
+    <?php include("footer.php") ?>
+    <!--  -->
+
+
+    <!-- Background -->
+    <div class="bgclass">
+        <div class="gradient"></div>
+    </div>
+    <!--  -->
+
+    <!-- Scripts -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="scripts/sidebarscript.js"></script>
+</body>
+
+</html>
