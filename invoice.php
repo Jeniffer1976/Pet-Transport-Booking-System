e100be2005a752ce21c090a0598fb5984b31ee88
LeaDeLeaf <104687952+LeaDeLeaf@users.noreply.github.com>
2023-08-06 00:40:49 +0800

    Users can receive invoice one time thru whatsapp

--- invoice.php
+++ invoice.php
@@ -1,5 +1,11 @@
 <?php
+
+include('dbFunctions.php');
+
 use Invoice\Order;
+use Twilio\Rest\Client;
+
+require_once 'vendor/autoload.php';
 
 require_once __DIR__ . '/Model/Order.php';
 $orderModel = new Order();
@@ -7,9 +13,39 @@ $id = $_GET["quote_id"];
 
 $orderItemResult = $orderModel->getQuote($id);
 $result = $orderModel->getPetOwners($orderItemResult[0]["owner_id"]);
+$recipientPhoneNo = "whatsapp:+65" . strval($result[0]['mobile']);
 
-if (! empty($result)) {
+$invoiceResult = $orderModel->getInvoice($orderItemResult[0]["quote_id"]);
+$messageStatus = $invoiceResult[0]['msgStatus'];
+$invoice_id = $invoiceResult[0]['invoice_id'];
+
+if (!empty($result)) {
     require_once __DIR__ . "/lib/PDFService.php";
     $pdfService = new PDFService();
     $pdfService->generatePDF($result, $orderItemResult);
+
+    // Update the path below to your autoload.php,
+    // see https://getcomposer.org/doc/01-basic-usage.md
+
+    $account_sid = "ACa8bd31122a50ac8ddb1f4b4fbe20ac03";
+    $token = "8bcd08290de197e76d1981654aee9734";
+    $twilio = new Client($account_sid, $token);
+
+    if ($messageStatus == 'NOT SEND') {
+        $message = $twilio->messages
+            ->create(
+                $recipientPhoneNo,
+                // to
+                array(
+                    "from" => "whatsapp:+14155238886",
+                    "body" => "Your quote has been reviewed and accepted by us. Here are the details of your invoice: https://f30d-121-6-10-155.ngrok-free.app/Pet-Transport-Booking-System/invoice.php?quote_id=" . $id
+                )
+            );
+
+        print($message->sid);
+
+        $updateMsgStat = "UPDATE invoice SET msgStatus='SEND' WHERE invoice_id = '$invoice_id'";
+        $msgStatUpdateStatus = mysqli_query($link, $updateMsgStat);
+    }
+
 }
--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -6,6 +6,20 @@ namespace Composer\Autoload;
 
 class ComposerStaticInit3dc378a0e49deba33c349752e7f39000
 {
+    public static $prefixLengthsPsr4 = array (
+        'T' => 
+        array (
+            'Twilio\\' => 7,
+        ),
+    );
+
+    public static $prefixDirsPsr4 = array (
+        'Twilio\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/twilio/sdk/src/Twilio',
+        ),
+    );
+
     public static $classMap = array (
         'Composer\\InstalledVersions' => __DIR__ . '/..' . '/composer/InstalledVersions.php',
         'Datamatrix' => __DIR__ . '/..' . '/tecnickcom/tcpdf/include/barcodes/datamatrix.php',
@@ -27,6 +41,8 @@ class ComposerStaticInit3dc378a0e49deba33c349752e7f39000
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
+            $loader->prefixLengthsPsr4 = ComposerStaticInit3dc378a0e49deba33c349752e7f39000::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInit3dc378a0e49deba33c349752e7f39000::$prefixDirsPsr4;
             $loader->classMap = ComposerStaticInit3dc378a0e49deba33c349752e7f39000::$classMap;
 
         }, null, ClassLoader::class);
