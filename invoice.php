d785af5d301d71cd25f3070174ed10d0ed0c14bf
LeaDeLeaf <104687952+LeaDeLeaf@users.noreply.github.com>
2023-08-07 13:35:53 +0800

    Fixed message whatsapp

--- invoice.php
+++ invoice.php
@@ -3,9 +3,9 @@
 include('loginFunctions.php');
 
 use Invoice\Order;
-use Twilio\Rest\Client;
 
 require_once 'vendor/autoload.php';
+use Twilio\Rest\Client;
 
 require_once __DIR__ . '/Model/Order.php';
 $orderModel = new Order();
@@ -29,24 +29,42 @@ if (!empty($result)) {
     $recipientPhoneNo = "whatsapp:+65" . strval($result[0]['mobile']);
     //$body_msg = 'Test message';
     //$body_msg = "Your quote has been reviewed and accepted by us. Here are the details of your invoice: https://f30d-121-6-10-155.ngrok-free.app/Pet-Transport-Booking-System/invoice.php?quote_id=" . strval($id);
-    $body_msg = 'Your quote from Waggin Wheels has been reviewed and accepted. Ensure you pay by the date stated in the invoice. Here are the invoice details: https://f30d-121-6-10-155.ngrok-free.app/Pet-Transport-Booking-System/invoice.php?quote_id=' . strval($id);
+    //$body_msg = 'Your quote from Waggin Wheels has been reviewed and accepted. Ensure you pay by the date stated in the invoice. Here are the invoice details: https://f30d-121-6-10-155.ngrok-free.app/Pet-Transport-Booking-System/invoice.php?quote_id=';
 
-    $account_sid = "ACa8bd31122a50ac8ddb1f4b4fbe20ac03";
-    $token = "8bcd08290de197e76d1981654aee9734";
-    $twilio = new Client($account_sid, $token);
+    // $sid    = "ACa8bd31122a50ac8ddb1f4b4fbe20ac03";
+    // $token  = "8416128f5e3ee8242cb13ae725765226";
+    // $twilio = new Client($account_sid, $token);
 
+    // if ($messageStatus == 'NOT SEND') {
+        // $message = $twilio->messages
+        //     ->create(
+        //         "whatsapp:+6581572740",
+        //         // to
+        //         array(
+        //             "from" => "whatsapp:+14155238886",
+        //             //"body" => "Your quote has been reviewed and accepted by us. Here are the details of your invoice: https://231a-121-6-10-155.ngrok-free.app/Pet-Transport-Booking-System/invoice.php?quote_id=".$id
+        //             "body" => 'Your Yummy Cupcakes Company order of 1 dozen frosted cupcakes has shipped and should be delivered on July 10, 2019. Details: http://www.yummycupcakes.com/'
+        //             //"body" => $body_msg
+        //         )
+        //     );
+
+        // print($message->sid);
+
+    //     $updateMsgStat = "UPDATE invoice SET msgStatus='SEND' WHERE invoice_id = '$invoice_id'";
+    //     $msgStatUpdateStatus = mysqli_query($link, $updateMsgStat);
+    
     if ($messageStatus == 'NOT SEND') {
+        $sid    = "ACa8bd31122a50ac8ddb1f4b4fbe20ac03";
+        $token  = "8416128f5e3ee8242cb13ae725765226";
+        $twilio = new Client($sid, $token);
+
         $message = $twilio->messages
-            ->create(
-                $recipientPhoneNo,
-                // to
-                array(
-                    "from" => "whatsapp:+14155238886",
-                    //"body" => "Your quote has been reviewed and accepted by us. Here are the details of your invoice: https://f30d-121-6-10-155.ngrok-free.app/Pet-Transport-Booking-System/invoice.php?quote_id=".$id
-                    //"body" => 'Your Yummy Cupcakes Company order of 1 dozen frosted cupcakes has shipped and should be delivered on July 10, 2019. Details: http://www.yummycupcakes.com/'
-                    "body" => $body_msg
-                )
-            );
+        ->create($recipientPhoneNo, // to
+            array(
+            "from" => "whatsapp:+14155238886",
+            "body" => "Your quote has been reviewed and accepted by us. Here are the details of your invoice: https://231a-121-6-10-155.ngrok-free.app/Pet-Transport-Booking-System/invoice.php?quote_id=".$id
+            )
+        );
 
         print($message->sid);
 
